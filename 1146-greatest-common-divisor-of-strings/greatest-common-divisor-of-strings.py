410a066743bfd1b880fe5cad4c147e0aa454d120
monirul8888 <monirul429588@gmail.com>
2025-06-01 00:45:51 +0600

    Time: 4 ms (25.84%) | Memory: 12.4 MB (56.32%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        
+        if((str1+str2)!=(str2+str1)):
+            return ""
+        else:
+            n1=len(str1)
+            n2=len(str2)
+            
+            while(n2!=0):
+                r=n1%n2
+                n1=n2
+                n2=r
+            gcd=n1
+
+            return(str1[:gcd])
+
+        
