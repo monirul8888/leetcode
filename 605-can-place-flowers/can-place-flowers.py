7c64be989032fb4c4620a335947e06c208166c22
monirul8888 <monirul429588@gmail.com>
2025-06-01 19:35:51 +0600

    Time: 7 ms (83.01%) | Memory: 13.3 MB (12.37%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -2,7 +2,6 @@ class Solution(object):
     def canPlaceFlowers(self, flowerbed, n):
         if n == 0:
             return True  
-
         for i in range(len(flowerbed)):
             if (flowerbed[i] == 0 and
                 (i == 0 or flowerbed[i - 1] == 0) and
